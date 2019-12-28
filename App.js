ffac09dbd4a04c1aaaf1af66fc194e9fc742e1bb
hosein-yarzade <hosein.yarzade@gmail.com>
2019-12-29 01:44:05 +0330

    can search device

--- App.js
+++ App.js
@@ -1,49 +1,96 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, PermissionsAndroid, TouchableOpacity} from 'react-native';
+import {BleManager} from 'react-native-ble-plx';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export async function requestLocationPermission() {
+    try {
+        const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION, {
+                title: 'Location permission for bluetooth scanning',
+                message: 'wahtever',
+                buttonNeutral: 'Ask Me Later',
+                buttonNegative: 'Cancel',
+                buttonPositive: 'OK',
+            },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            console.log('Location permission for bluetooth scanning granted');
+            return true;
+        } else {
+            console.log('Location permission for bluetooth scanning revoked');
+            return false;
+        }
+    } catch (err) {
+        console.warn(err);
+        return false;
+    }
+}
 
-type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+
+    constructor() {
+        super();
+        this.manager = new BleManager();
+    }
+
+    componentWillMount() {
+
+    }
+
+    allowConnect = () => {
+        requestLocationPermission()
+    };
+    scanAndConnect = () => {
+
+        this.manager.startDeviceScan(null, null, (error, device) => {
+            if (error) {
+                // Handle error (scanning will be stopped automatically)
+                alert('error', error);
+                console.log(error);
+                return
+            }
+
+            // Check if it is a device you are looking for based on advertisement data
+            // or other criteria.
+            if (device.name === 'TI BLE Sensor Tag' ||
+                device.name === 'SensorTag') {
+                // Stop scanning as it's not necessary if you are scanning for one device.
+                this.manager.stopDeviceScan();
+                // Proceed with connection.
+            }
+        });
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>Welcome to React Native!</Text>
+                <TouchableOpacity onPress={this.allowConnect}>
+                    <Text>Allow</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this.scanAndConnect}>
+                    <Text>search BlueTooth</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
